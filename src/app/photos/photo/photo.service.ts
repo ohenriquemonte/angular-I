7fd3771632155cadd3d49f15f7a585997c114c2b
Henrique Montenegro <montenegrohenrique@MonteBook.local>
2019-09-22 22:51:04 -0300

    Modulo 06 - Melhorando a experiência do usuário

--- src/app/photos/photo/photo.service.ts
+++ src/app/photos/photo/photo.service.ts
@@ -1,5 +1,5 @@
 import { Photo } from './photo';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 const API = 'http://localhost:3000';
@@ -10,7 +10,15 @@ export class PhotoService {
 
 	listFromUser(userName: string) {
 		return this.http
-			// .get<Photo[]>(API + '/' + userName + '/photos');
 			.get<Photo[]>(`${API}/${userName}/photos`);
+	}
+
+	listFromUserPaginated(userName: string, page: number) {
+		const params = new HttpParams()
+			.append('page', page.toString());
+		return this.http
+			.get<Photo[]>(`${API}/${userName}/photos`, {
+				params: params
+			});
 	}
 }
