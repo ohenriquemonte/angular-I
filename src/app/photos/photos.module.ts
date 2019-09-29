271cc39f9e5eaf89fcf7390771db398c983fe93a
Henrique Montenegro <montenegrohenrique@MonteBook.local>
2019-09-29 12:23:48 -0300

    Modulo 07.2 - Submodulos

--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -1,28 +1,18 @@
-import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
-import { PhotoComponent } from './photo/photo.component';
-import { PhotoListComponent } from './photo-list/photo-list.component';
-import { PhotoFormComponent } from './photo-form/photo-form.component';
-import { PhotosComponent } from './photo-list/photos/photos.component';
-import { FilterByDescription } from './photo-list/filter-by-description.pipe';
-import { LoadButtonComponent } from './photo-list/load-button/load-button.component';
+import { PhotoListModule } from './photo-list/photo-list.module';
+import { PhotoFormModule } from './photo-form/photo-form.module';
+import { PhotoModule } from './photo/photo.module';
 
 
 @NgModule({
-	declarations: [
-		PhotoComponent,
-		PhotoListComponent,
-		PhotoFormComponent,
-		PhotosComponent,
-		FilterByDescription,
-		LoadButtonComponent
-	],
 	imports: [
-		HttpClientModule,
-		CommonModule
+		PhotoModule,
+		PhotoFormModule,
+		PhotoListModule
 	]
 })
-export class PhotosModule { }
 
+export class PhotosModule {
+
+}
